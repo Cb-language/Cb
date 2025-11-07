9a3992b85e8a949478416204bc014d4b7f600b49
Itay-He <hershkoitai@gmail.com>
2025-11-07 14:54:21 +0200

    Added types in TokenType

--- Transposer/Token.h
+++ Transposer/Token.h
@@ -6,6 +6,22 @@ typedef unsigned char byte;
 
 enum TokenType : byte
 {
+	IDENTIFIAR = 1,
+	TYPE,
+	KEYWORD,
+	PUNCTUATION,
+	OP_UNARY,
+	OP_BINARY,
+	OP_ASSIGNMENT,
+
+	CONST_INT = 10,
+	CONST_FLOAT,
+	CONST_CHAR,
+	CONST_BOOL,
+	CONST_STR,
+
+	COMMENT_SINGLE = 20,
+	COMMENT_MULTI
 };
 
 struct Token
