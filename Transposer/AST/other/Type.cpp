20adb42ef1941875032117507c91000b3760dbf9
Itay-He <hershkoitai@gmail.com>
2025-11-14 14:28:56 +0200

    Made Type

--- Transposer/AST/other/Type.cpp
+++ Transposer/AST/other/Type.cpp
@@ -1,1 +1,46 @@
 #include "Type.h"
+#include <utility>
+
+const std::unordered_map<std::wstring, std::string> Type::typeMap = {
+    {L"bar",     "std::string"}, // String
+    {L"note",    "char"},        // Char
+    {L"mute",    "bool"},        // Bool
+    {L"freq",    "double"},      // Double
+    {L"degree",  "int"},         // Int
+    //{L"scale",   ""}, // Array - TODO: add this in sprint 3
+    {L"fermata", "void"}         // Void
+};
+
+const std::unordered_map<std::wstring, std::unordered_set<std::wstring>> Type::castMap = {
+    {L"bar",     {L"bar"}},                          // std::string → only string
+    {L"note",    {L"note", L"flat", L"freq"}},      // char → int, double
+    {L"mute",    {L"mute", L"flat", L"freq"}},      // bool → int, double
+    {L"sharp",   {L"sharp", L"flat", L"freq"}},     // unsigned → int, double
+    {L"flat",    {L"flat", L"freq"}},               // int → double
+    {L"degree",  {L"degree", L"flat", L"freq"}},    // int → double
+    {L"freq",    {L"freq"}},                        // double → double
+//  {L"scale",   {L"scale"}},                      // Array - TODO: add this in sprint 3
+    {L"fermata", {}}                                // void → nothing
+};
+
+Type::Type(std::wstring  type) : type(std::move(type))
+{
+}
+
+bool Type::operator==(const Type& other) const
+{
+    const auto it = castMap.find(other.type);
+    if (it == castMap.end() && type != other.type) return false; // the second is for future (class)
+    return it->second.contains(other.type);
+}
+
+bool Type::operator!=(const Type& other) const
+{
+    return !(*this == other);
+}
+
+std::ostream& operator<<(std::ostream& os, const Type& t)
+{
+    os << Type::typeMap.at(t.type);
+    return os;
+}
--- Transposer/AST/other/Type.h
+++ Transposer/AST/other/Type.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 // Not a statement
 class Type
 {
 private:
     const std::wstring type;
+    static const std::unordered_map<std::wstring, std::string> typeMap;
+    static const std::unordered_map<std::wstring, std::unordered_set<std::wstring>> castMap;
 
 public:
-    explicit Type(const std::wstring& type);
+    explicit Type(std::wstring  type);
     bool operator==(const Type& other) const;
     bool operator!=(const Type& other) const;
     friend std::ostream& operator<<(std::ostream& os, const Type& t);
