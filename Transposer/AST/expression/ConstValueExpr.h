c7a616aebf46bf80f786ab644d48b8f5fba38d54
elisha schafman <elisha.schafman@gmail.com>
2025-11-15 17:31:36 +0200

    added const value expressions

---
+++ Transposer/AST/expression/ConstValueExpr.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "AST/abstract/Expression.h"
+
+class ConstValueExpr : public Expression
+{
+private:
+    Type type;
+    std::string value;
+public:
+    ConstValueExpr(const int line, const int column, const Type &type, const std::string &value);
+    Type getType() const override;
+    bool isLegal() const override;
+};
