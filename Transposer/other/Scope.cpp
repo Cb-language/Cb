04f41d1605f6b595a56243dab367deb5f94922ed
Itay-He <hershkoitai@gmail.com>
2025-11-18 14:59:04 +0200

    Made Scope

---
+++ Transposer/other/Scope.cpp
@@ -0,0 +1,58 @@
+#include "Scope.h"
+
+#include "errorHandling/lexicalErrors/IdentifierTaken.h"
+#include "errorHandling/syntaxErrors/UnexpectedToken.h"
+
+Scope::Scope(Scope* parent) : parent(parent)
+{
+}
+
+bool Scope::doesExist(const Var v) const
+{
+    for (const auto& var : vars)
+    {
+        if (v == var)
+        {
+            return true;
+        }
+    }
+
+    if (parent != nullptr)
+    {
+        return parent->doesExist(v);
+    }
+
+    return false;
+}
+
+Scope* Scope::makeNewScope()
+{
+    auto s = std::make_unique<Scope>(this);
+    Scope* raw = s.get();       // keep raw pointer
+    children.push_back(std::move(s));
+    return raw;
+}
+
+Scope* Scope::getParent() const
+{
+    return parent;
+}
+
+void Scope::addVar(const Type type, const Token& token)
+{
+    if (token.type != Token::IDENTIFIER)
+    {
+        throw UnexpectedToken(token);
+    }
+
+    const Var v = Var(type , token.value);
+    for (const auto& var : vars)
+    {
+        if (v == var)
+        {
+            throw IdentifierTaken(token);
+        }
+    }
+
+    vars.push_back(v);
+}
---
+++ Transposer/other/Scope.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+#include "Var.h"
+#include "token/Token.h"
+
+class Scope
+{
+private:
+    std::vector<Var> vars;
+    Scope* parent;
+    std::vector<std::unique_ptr<Scope>> children;
+
+public:
+    Scope(Scope* parent = nullptr);
+    bool doesExist(const Var v) const;
+
+    Scope* makeNewScope();
+    Scope* getParent() const;
+    void addVar(const Type type, const Token& token);
+};
