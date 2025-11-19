d933a74a4850c8f00147ec9619b1269d8308eb6d
Itay-He <hershkoitai@gmail.com>
2025-11-19 09:51:35 +0200

    Updated ConstValueExpr::translateToCpp()

--- Transposer/AST/statements/expression/ConstValueExpr.cpp
+++ Transposer/AST/statements/expression/ConstValueExpr.cpp
@@ -1,8 +1,11 @@
+//
+// Created by USER on 11/15/2025.
+//
+
 #include "ConstValueExpr.h"
 
 
-ConstValueExpr::ConstValueExpr(const Type& type, const std::wstring& value) : type(type), value(value)
-{
+ConstValueExpr::ConstValueExpr(const int line, const int column, const Type &type, const std::wstring &value) : Expr(line, column), type(type), value(value) {
 }
 
 bool ConstValueExpr::isLegal() const {
@@ -14,6 +17,11 @@ bool ConstValueExpr::isLegal() const {
 }
 
 std::string ConstValueExpr::translateToCpp() const {
+    if (type.getType() == L"freq" && value.starts_with(L"."))
+    {
+        return Utils::wstrToStr(L"0" + value);
+    }
+
     return Utils::wstrToStr(value);
 }
 
