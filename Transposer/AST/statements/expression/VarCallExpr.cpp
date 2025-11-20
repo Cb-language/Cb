6cc596b564774737bd266a0b732a8fed36e8a2f6
Itay-He <hershkoitai@gmail.com>
2025-11-20 20:38:34 +0200

    Added VarCallExpr

---
+++ Transposer/AST/statements/expression/VarCallExpr.cpp
@@ -0,0 +1,20 @@
+#include "VarCallExpr.h"
+
+VarCallExpr::VarCallExpr(const Var& var) : var(var)
+{
+}
+
+bool VarCallExpr::isLegal() const
+{
+    return true;
+}
+
+std::string VarCallExpr::translateToCpp() const
+{
+    return Utils::wstrToStr(var.getName());
+}
+
+Type VarCallExpr::getType() const
+{
+    return var.getType();
+}
