b1561823f4b7e005ff36e33c70ec0f1476cadf5a
Elisha Schafman <elisha.schafman@gmail.com>
2025-11-30 17:55:52 +0200

    made bodystmt extend stmt

--- Transposer/AST/statements/BodyStmt.h
+++ Transposer/AST/statements/BodyStmt.h
@@ -2,11 +2,15 @@
 
 #include "../Statements.h"
 
-class BodyStmt
+class BodyStmt : public Stmt
 {
 private:
     std::vector<std::unique_ptr<Stmt>> stmts;
 public:
+    BodyStmt(const std::vector<std::unique_ptr<Stmt>>& stmts);
     const std::vector<std::unique_ptr<Stmt>>& getStmts() const;
+
+    bool isLegal() const override;
+    std::string translateToCpp() override;
 
 };
