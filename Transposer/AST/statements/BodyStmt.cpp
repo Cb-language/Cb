242c47e579e49fedc445bd9f1e5f3768f09c8f63
Elisha Schafman <elisha.schafman@gmail.com>
2025-11-30 18:01:34 +0200

    added isLegal function

--- Transposer/AST/statements/BodyStmt.cpp
+++ Transposer/AST/statements/BodyStmt.cpp
@@ -3,7 +3,7 @@
 BodyStmt::BodyStmt(Scope* scope, FuncDeclStmt* funcDecl, std::vector<std::unique_ptr<Stmt>>& stmts) :
     Stmt(scope, funcDecl)
 {
-    for (const auto& s : stmts)
+    for (auto& s : stmts)
     {
         this->stmts.push_back(std::move(s));
     }
@@ -12,4 +12,16 @@ BodyStmt::BodyStmt(Scope* scope, FuncDeclStmt* funcDecl, std::vector<std::unique
 const std::vector<std::unique_ptr<Stmt>>& BodyStmt::getStmts() const
 {
     return stmts;
+}
+
+bool BodyStmt::isLegal() const
+{
+    for (const auto& s : stmts)
+    {
+        if (!s->isLegal())
+        {
+            return false;
+        }
+    }
+    return true;
 }
