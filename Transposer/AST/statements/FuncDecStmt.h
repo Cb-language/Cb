90044e2464612be9719c2fbea777b428d5cefedf
elisha schafman <elisha.schafman@gmail.com>
2025-12-02 00:59:53 +0200

    added getters and translateToCpp

--- Transposer/AST/statements/FuncDecStmt.h
+++ Transposer/AST/statements/FuncDecStmt.h
@@ -6,5 +6,15 @@ class FuncDecStmt : public Stmt
 {
 private:
     std::vector<std::unique_ptr<Var>> params;
-    std::
+    std::string funcName;
+    Type returnType;
+public:
+    FuncDecStmt(Scope* scope, FuncDeclStmt* funcDecl, const std::string &funcName, const Type &returnType, std::vector<std::unique_ptr<Var>> &&params);
+
+    std::vector<std::unique_ptr<Var>> getParams() const;
+    std::string getName() const;
+    Type getReturnType() const;
+
+    bool isLegal() const override;
+    std::string translateToCpp() const override;
 };
