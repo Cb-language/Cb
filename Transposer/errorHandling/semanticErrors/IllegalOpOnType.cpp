797c942e01cc2161d44d725cf60a3555190230a0
Itay-He <hershkoitai@gmail.com>
2026-01-03 16:33:01 +0200

    Updated IllegalOpOnType

--- Transposer/errorHandling/semanticErrors/IllegalOpOnType.cpp
+++ Transposer/errorHandling/semanticErrors/IllegalOpOnType.cpp
@@ -3,3 +3,8 @@
 IllegalOpOnType::IllegalOpOnType(const Token& token, const std::string& type) : Error(token, "Can't do that op on " + type)
 {
 }
+
+IllegalOpOnType::IllegalOpOnType(const Token& token, const std::string& type1, const std::string& type2,
+    const std::string& op) : Error(token, "Can't do \"" + op + "\" on types: " + type1 + ", " + type2)
+{
+}
--- Transposer/errorHandling/semanticErrors/IllegalOpOnType.h
+++ Transposer/errorHandling/semanticErrors/IllegalOpOnType.h
@@ -5,4 +5,5 @@ class IllegalOpOnType : public Error
 {
 public:
     IllegalOpOnType(const Token& token, const std::string& type);
+    IllegalOpOnType(const Token& token, const std::string& type1, const std::string& type2, const std::string& op);
 };
