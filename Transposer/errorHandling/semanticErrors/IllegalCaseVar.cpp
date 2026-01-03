f7c21a255a2528d2a20f9222d0e740bdb8e568bb
Itay-He <hershkoitai@gmail.com>
2026-01-03 15:53:14 +0200

    Changed IllegalCaseVar to IllegalSwitchVar

--- Transposer/errorHandling/semanticErrors/IllegalCaseVar.cpp
+++
@@ -1,5 +0,0 @@
-#include "IllegalCaseVar.h"
-
-IllegalCaseVar::IllegalCaseVar(const Token& token, const std::string& name) : Error(token, "Case must have the variable (" + name + ") to be degreeable")
-{
-}
--- Transposer/errorHandling/semanticErrors/IllegalCaseVar.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "errorHandling/Error.h"
-
-class IllegalCaseVar : public Error
-{
-public:
-    IllegalCaseVar(const Token& token, const std::string& name);
-};
