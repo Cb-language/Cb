e2bbe7c2dc41a554b0e08eff8ad7a556ab35f95e
Itay-He <hershkoitai@gmail.com>
2026-02-13 15:23:27 +0200

    Made ClassNode

---
+++ Transposer/Transposer/class/ClassNode.cpp
@@ -0,0 +1,72 @@
+#include "ClassNode.h"
+
+ClassNode::ClassNode(const Class& c, ClassNode* parent) : parent(parent), c(c.copy())
+{
+}
+
+ClassNode::~ClassNode()
+{
+    for (const auto& child : children)
+    {
+        delete child;
+    }
+
+    children.clear();
+    parent = nullptr;
+}
+
+bool ClassNode::isLegal(const Var& field, const ClassNode* curr) const
+{
+    if (parent != nullptr && parent->isLegal(field, curr)) return true;
+
+    for (const auto& [isPublic, f] : c.getFields())
+    {
+        if (curr != this && !isPublic) continue;
+        if (f == field) return true;
+    }
+    return false;
+}
+
+bool ClassNode::isLegal(const Func& method, const ClassNode* curr) const
+{
+    if (parent != nullptr && parent->isLegal(method, curr)) return true;
+
+    for (const auto& [isPublic, m] : c.getMethods())
+    {
+        if (curr != this && !isPublic) continue;
+        if (m == method) return true;
+    }
+    return false;
+}
+
+bool ClassNode::isLegal(const Constractor& ctor, const ClassNode* curr) const
+{
+    if (parent != nullptr && parent->isLegal(ctor, curr)) return true;
+
+    for (const auto& [isPublic, ct] : c.getConstractors())
+    {
+        if (curr != this && !isPublic) continue;
+        if (ct == ctor) return true;
+    }
+    return false;
+}
+
+void ClassNode::add(const bool isPublic, const Var& field)
+{
+    c.addField(isPublic, field);
+}
+
+void ClassNode::add(const bool isPublic, const Func& method)
+{
+    c.addMethod(isPublic, method);
+}
+
+void ClassNode::add(const bool isPublic, const Constractor& ctor)
+{
+    c.addConstractor(isPublic, ctor);
+}
+
+void ClassNode::addChild(const Class& cl)
+{
+    children.emplace_back(new ClassNode(cl, this));
+}
---
+++ Transposer/Transposer/class/ClassNode.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "AST/statements/ClassDeclStmt.h"
+#include "symbols/Class.h"
+
+class ClassNode
+{
+private:
+    ClassNode* parent;
+    Class c;
+    std::vector<ClassNode*> children;
+
+public:
+    explicit ClassNode(const Class& c, ClassNode* parent = nullptr);
+    ~ClassNode();
+
+    bool isLegal(const Var& field, const ClassNode* curr) const;
+    bool isLegal(const Func& method, const ClassNode* curr) const;
+    bool isLegal(const Constractor& ctor, const ClassNode* curr) const;
+
+    void add(const bool isPublic, const Var& field);
+    void add(const bool isPublic, const Func& method);
+    void add(const bool isPublic, const Constractor& ctor);
+
+    void addChild(const Class& cl);
+};
