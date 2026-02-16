a5ef2c7f28bfb9051c7b7eb01de8a644efc8067a
Itay-He <hershkoitai@gmail.com>
2026-02-16 13:40:42 +0200

    Deleted output files

--- Transposer/test files/MultiFilesTests/B.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "//home/schafiii/CLionProjects/kiryatgat-1401-c/Transposer/test files/MultiFilesTests/B.h"
-
-void b()
-{
-	std::cout << "B.cb" << std::endl;
-	c();
-}
--- Transposer/test files/MultiFilesTests/D.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "//home/schafiii/CLionProjects/kiryatgat-1401-c/Transposer/test files/MultiFilesTests/D.h"
-
-void d()
-{
-	std::cout << "D.cb" << std::endl;
-}
